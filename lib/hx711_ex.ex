ca6b59fdc8290c91761f693364e7dda65d9780c8
Oskar Niburski <oskarniburski@gmail.com>
2020-02-17 21:54:42 -0800

    feat(init): Init

---
+++ lib/hx711_ex.ex
@@ -0,0 +1,18 @@
+defmodule Hx711Ex do
+  @moduledoc """
+  Documentation for Hx711Ex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hx711Ex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hx711_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule Hx711ExTest do
+  use ExUnit.Case
+  doctest Hx711Ex
+
+  test "greets the world" do
+    assert Hx711Ex.hello() == :world
+  end
+end
